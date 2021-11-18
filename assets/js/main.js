d0b54cfcfd17111abf5196b0570ff5927e4a6901
Semko1010 <semir01020@gmail.com>
2021-11-18 16:17:02 +0100

    v1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,26 +5,46 @@ let sieben = document.getElementById("7%");
 let input = document.getElementById("input");
 let mwsBetrag = document.getElementById("mwsBetrag");
 let endPreis = document.getElementById("endPreis");
+let nettoBrutto = document.getElementById("nettoBrutto");
 
 
-berechnen = () => {
+const berechnen = () => {
     if (aufschlagen.checked && neunZehn.checked) {
-        endPreis.innerHTML = Number(input.value) * 1.19;
-        let test = Number(input.value) * 1.19;
-        mwsBetrag.innerHTML = test - input.value;
+        let endergebnis = Number(input.value) * 1.19;
+        endPreis.innerHTML = endergebnis.toFixed(2) + "€";
+        let differenz = endergebnis - input.value
+        mwsBetrag.innerHTML = differenz.toFixed(2) + "€";
 
     } else if (aufschlagen.checked && sieben.checked) {
-        endPreis.innerHTML = Number(input.value) * 1.07;
-        let test = Number(input.value) * 1.07;
-        mwsBetrag.innerHTML = test - input.value;
+        let endergebnis2 = Number(input.value) * 1.07;
+        endPreis.innerHTML = endergebnis2.toFixed(2) + "€";
+        let differenz2 = endergebnis2 - input.value
+        mwsBetrag.innerHTML = differenz2 - input.value + "€";
     } else if (abziehen.checked && neunZehn.checked) {
-        endPreis.innerHTML = Number(input.value) - 1.19;
-        let test = Number(input.value) - 1.19;
-        mwsBetrag.innerHTML = test - input.value;
+
+        let endergebnis3 = Number(input.value) / 1.19;
+        endPreis.innerHTML = endergebnis3.toFixed(2) + "€";
+        let differenz3 = input.value - endergebnis3;
+        mwsBetrag.innerHTML = differenz3.toFixed(2) + "€";
+
+
     } else if (abziehen.checked && sieben.checked) {
-        endPreis.innerHTML = Number(input.value) - 1.07;
-        let test = Number(input.value) - 1.07;
-        mwsBetrag.innerHTML = test - input.value;
+        let endergebnis4 = Number(input.value) / 1.07;
+        endPreis.innerHTML = endergebnis4.toFixed(2) + "€";
+        let differenz4 = input.value - endergebnis4;
+        mwsBetrag.innerHTML = differenz4.toFixed(2) + "€";
     }
 
+
+}
+
+const abziehenF = () => {
+    if (abziehen.checked) {
+        nettoBrutto.innerHTML = "Bruttobetrag (Preis inklusive Mehrwertsteuer) in Euro"
+    }
+}
+const aufschlagenF = () => {
+    if (aufschlagen.checked) {
+        nettoBrutto.innerHTML = "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro"
+    }
 }
